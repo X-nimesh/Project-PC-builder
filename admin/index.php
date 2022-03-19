f0f5a8ccf27486cd39ae4ae2560ce5e0dca08034
IroshaThapa <iroshamagar@gmail.com>
2022-03-19 13:55:05 +0545

    iru

--- admin/index.php
+++ admin/index.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style1.css">
     <title>Admin Login</title>
 </head>
 <body>
-    <form action="adminBack.php">
+    <form action="adminBack.php" class="adminLogin">
         username: <input type="text" name="username" id="username"><br>
         password: <input type="password" name="password" id="password"><br>
         <input type="submit" value="Login">
