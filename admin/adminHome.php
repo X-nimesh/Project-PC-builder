038f35c47f0c7fa5e71ddf124ca1f2338834414f
X-nimesh <nimush14@gmail.com>
2022-03-26 22:32:38 +0545

    admin opage halka change

--- admin/adminHome.php
+++ admin/adminHome.php
@@ -8,22 +8,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style1.css">
     <title>Admin Page</title>
 </head>
 <body>
-   
-    <div class="sidebarA">
-        <ul>
-            <li>Orders</li>
-            <li>Users</li>
-            <li>Vendors</li>
-            <li>Account</li>
-        </ul>
+   <div class="body">
+        <div class="sidebarA">
+            <ul>
+                <li>Orders</li>
+                <li>Users</li>
+                <li>Vendors</li>
+                <li>Account</li>
+            </ul>
+        </div>
+        <div class="mainA">
+            <h1>Hello Admin!</h1>
+            <p>Welcome to the admin page!</p>
+        </div>
     </div>
-    <div class="mainA">
-        
-    </div>
-
     <script >
         // let logged_in='';
         // console.log(logged_in);
