86f1df219f35f88f7bfa3f55babf72eab219dcef
X-nimesh <nimush14@gmail.com>
2022-04-27 14:21:55 +0545

    a

--- vendor/vendorNavBar.php
+++ vendor/vendorNavBar.php
@@ -4,7 +4,7 @@
             <ul>
                 <li class="dashboard" ><a  href="home.php"> Dashboard</a></li>
                 <li class="products" ><a  href="products.php"> Products</a></li>
-                <!-- <li class="orders" ><a  href="orders.php"> Orders</a></li> -->
+                <li class="orders" ><a  href="orders.php"> Orders</a></li>
                 <li class="profile" ><a  href="profile.php"> Profile</a></li>
                 <li><div id="btn-main" class="loginB vendorLO"><a href="../server/logOut.php" id="btntxt">Log Out</a></div></li>
             </ul>
